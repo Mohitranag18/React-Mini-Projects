1cdf5dd6d3f7210ba995f9fce69ebc3873ba409d
Mohit Rana <mohitr8998@gmail.com>
2024-11-04 06:22:56 -0500

    added initial files for currency converter project

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-gray-900 flex justify-center items-center h-[100vh] w-[100vw]'>
+      <div className="container bg-slate-400 w-96 h-60">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
